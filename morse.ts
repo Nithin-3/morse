1bfe365aa2284612e28330c624f9335fb0ea42d3
Nithin-3 <nithin3dev@gmail.com>
2025-02-11 19:01:12 +0530

    TS add updated

---
+++ morse.ts
@@ -0,0 +1,31 @@
+MORSE_CODE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-', ' ': '/'
+}
+const reversedObj = Object.fromEntries(
+    Object.entries(MORSE_CODE).map(([key, value]) => [value, key])
+);
+const morseToTxt = (morse:String):String=>{
+    let txt ='';
+    morse.split(" ")
+        .forEach(e => {
+            txt += reversedObj[`${e}`] ?? e
+
+        });
+    return txt
+}
+const txtToMorse = (txt:String):String=>{
+    let morse ='';
+    txt.toUpperCase().split("").forEach(e => {
+        morse += MORSE_CODE[`${e}`]+" "
+    });
+    return morse.trim()
+}
+
+const morseOrTxt = process.argv.slice(2).join()
+console.log(`MorseCode To Text ${morseToTxt(morseOrTxt)}\nText to MorseCode ${txtToMorse(morseOrTxt)}`);
+
